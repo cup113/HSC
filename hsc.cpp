7f12b82b77149e882c85d7d49e8560c979c55ea5
Jason M. Li <jasonmli@qq.com>
2022-10-15 11:39:07 +0800

    v0.4.0 Improve code & modify description

--- hsc.cpp
+++ hsc.cpp
@@ -1,8 +1,8 @@
 /**
  * @file hsc.cpp Homework Showing Compiler
- * @version 0.3.1 (Beta)
+ * @version 0.4.0 (Beta)
  * @author Jason M. Li
- * @date 2022.10.5
+ * @date 2022.10.15
  */
 
 #include <cstdio>
@@ -16,7 +16,9 @@
 #include <ctime>
 #define LINE_BUF 4096
 
-enum SP_CH : short {
+using std::string;
+
+enum class SP_CH : short {
     META_DATE = 0, // 日期声明开始
     META_CREATED, // 创建声明开始
     META_MODIFIED, // 更正声明开始
@@ -34,70 +36,65 @@ enum SP_CH : short {
     ROOT // 根元素/占位符
 };
 
-class Tag {
-public:
-    short sp_ch = -1;
-    std::string temp_str = "";
+struct Tag {
+    SP_CH sp_ch = SP_CH::ROOT;
+    string temp_str = "";
     int temp_int = 0;
-    Tag(short _sp_ch) {
+    Tag(SP_CH _sp_ch) {
         this->sp_ch = _sp_ch;
     }
 };
 
-class Modification {
-public:
-    std::string bg_color = "yellow";
-    std::string modified_time = "--:--";
+struct Modification {
+    string bg_color = "yellow";
+    string modified_time = "--:--";
     Modification() {}
-    Modification(std::string _bg_color, std::string _modified_time) {
+    Modification(string _bg_color, string _modified_time) {
         this->bg_color = _bg_color;
         this->modified_time = _modified_time;
     }
 };
 
-class Item {
-public:
-    std::string text;
+struct Item {
+    string text;
     int number;
     Item() {}
-    Item(std::string _text) {
+    Item(string _text) {
         this->text = _text;
     }
-    Item(std::string _text, int _number) {
+    Item(string _text, int _number) {
         this->text = _text;
         this->number = _number;
     }
 };
 
-class Subject {
-public:
-    std::string name;
+struct Subject {
+    string name;
     std::vector<Item> items;
     Subject() {}
-    Subject(std::string _name) {
+    Subject(string _name) {
         this->name = _name;
     }
 };
 
-class OutputFile {
-public:
-    std::string date = "--------";
-    std::string created_time = "--:--";
-    std::string published_time = "--:--:--";
+struct OutputFile {
+    string date = "--------";
+    string created_time = "--:--";
+    string published_time = "--:--:--";
     std::vector<Modification> modified_times;
     std::vector<Subject> subjects;
     OutputFile() {}
 };
 
-std::string add_label(std::string text, std::string tagName) {
+string add_label(string text, string tagName) {
     return '<' + tagName + '>' + text + "</" + tagName + '>';
 }
 
-std::string add_label(std::string text, std::string tagName, std::map<std::string, std::string> attrs) {
-    std::string result = '<' + tagName;
+string add_label(string text, string tagName, std::map<string, string> attrs) {
+    string result = '<' + tagName;
     auto attr = attrs.begin();
     for (; attr != attrs.end(); attr++) {
-        std::string temp_attr = ' ' + attr->first + "=\"" + attr->second + '"';
+        string temp_attr = ' ' + attr->first + "=\"" + attr->second + '"';
         result += temp_attr;
     }
     result += ">";
@@ -111,7 +108,7 @@ int main(int argc, char* argv[]) {
         printf("Error 0x0001: No File Input.\n");
         return 0x0001;
     }
-    std::string filename(argv[1]);
+    string filename(argv[1]);
     std::ifstream reader;
     std::ofstream writer;
     reader.open(filename);
@@ -127,7 +124,7 @@ int main(int argc, char* argv[]) {
         number_counter = 0,
         line_counter = 0;
     char line_buffer[LINE_BUF];
-    std::string str_buffer;
+    string str_buffer;
     int line_len;
     while (reader.getline(line_buffer, LINE_BUF)) {
         line_len = strlen(line_buffer);
@@ -136,7 +133,7 @@ int main(int argc, char* argv[]) {
         {
             char first_ch = str_buffer[0];
             if (first_ch == '+') {
-                std::string next_five_ch = str_buffer.substr(1, 5);
+                string next_five_ch = str_buffer.substr(1, 5);
                 if (next_five_ch.compare("date ") == 0) {
                     tags.push(Tag(SP_CH::META_DATE));
                 }
@@ -173,7 +170,7 @@ int main(int argc, char* argv[]) {
         for (int i = 0; i < line_len; i++) {
             unsigned char ch = str_buffer[i];
             int tag_size = tags.size();
-            int top_sp_ch = tags.top().sp_ch;
+            SP_CH top_sp_ch = tags.top().sp_ch;
             if (tag_size == 0) {
                 printf("ERROR 0x0020: no tags left.\n");
                 return 0x0020;
@@ -239,7 +236,7 @@ int main(int argc, char* argv[]) {
                 tags.push(Tag(SP_CH::START_EMPHASIZE_TEXT));
             }
             else if (ch == '*' && top_sp_ch == SP_CH::START_EMPHASIZE_TEXT) {
-                std::string text = tags.top().temp_str;
+                string text = tags.top().temp_str;
                 tags.pop();
                 int version = tags.top().temp_int;
                 tags.pop();
@@ -247,9 +244,9 @@ int main(int argc, char* argv[]) {
                     tags.top().temp_str += add_label(text, "strong");
                 }
                 else {
-                    std::map<std::string, std::string> attrs;
-                    attrs["style"] = "--bgc: " + outfile.modified_times[version - 1].bg_color;
-                    tags.top().temp_str += add_label(text, "strong", attrs);
+                    tags.top().temp_str += add_label(text, "strong", { {
+                        "style", "--bgc: " + outfile.modified_times[version - 1].bg_color
+                    } });
                 }
             }
             // 超链接
@@ -262,21 +259,15 @@ int main(int argc, char* argv[]) {
             else if (ch == '(' && top_sp_ch == SP_CH::START_HYPERLINK && tags.top().temp_int == 1) {
                 tags.push(Tag(SP_CH::START_HYPERLINK_TEXT));
             }
-            else if (ch == ')' && top_sp_ch == SP_CH::START_HYPERLINK && tags.top().temp_int == 1) {
-                std::string url = tags.top().temp_str;
-                tags.pop();
-                std::map<std::string, std::string> attrs;
-                attrs["href"] = url;
-                tags.top().temp_str += add_label(url, "a", attrs);
-            }
             else if (ch == ')' && top_sp_ch == SP_CH::START_HYPERLINK_TEXT) {
-                std::string text = tags.top().temp_str;
+                string text = tags.top().temp_str;
                 tags.pop();
-                std::string url = tags.top().temp_str;
+                string url = tags.top().temp_str;
                 tags.pop();
-                std::map<std::string, std::string> attrs;
-                attrs["href"] = url;
-                tags.top().temp_str += add_label(text, "a", attrs);
+                if (text.size() == 0) {
+                    text = url;
+                }
+                tags.top().temp_str += add_label(text, "a", { {"href", url} });
             }
             // 空格分割的数据声明
             else if (ch == ' ' && top_sp_ch == SP_CH::META_MODIFIED) {
@@ -288,9 +279,9 @@ int main(int argc, char* argv[]) {
         }
         while (tags.top().sp_ch >= SP_CH::START_HYPERLINK && tags.top().sp_ch <= SP_CH::START_ESCAPE) {
             // 过滤堆栈未使用内容
-            std::string temp_str = tags.top().temp_str;
+            string temp_str = tags.top().temp_str;
             int temp_int = tags.top().temp_int;
-            short top_sp_ch = tags.top().sp_ch;
+            SP_CH top_sp_ch = tags.top().sp_ch;
             tags.pop();
             switch (top_sp_ch) {
             case (SP_CH::START_EMPHASIZE):
@@ -343,7 +334,7 @@ int main(int argc, char* argv[]) {
         }
         {
             // 监测堆栈/最近的行级项
-            int top_sp_ch = tags.top().sp_ch;
+            SP_CH top_sp_ch = tags.top().sp_ch;
             if (top_sp_ch == SP_CH::META_DATE) {
                 outfile.date = tags.top().temp_str;
                 tags.pop();
@@ -357,7 +348,7 @@ int main(int argc, char* argv[]) {
                 tags.pop();
             }
             else if (top_sp_ch == SP_CH::META_MODIFIED_COLOR) {
-                std::string color = tags.top().temp_str;
+                string color = tags.top().temp_str;
                 tags.pop();
                 outfile.modified_times.push_back(Modification(color, tags.top().temp_str));
                 tags.pop();
@@ -395,10 +386,10 @@ int main(int argc, char* argv[]) {
     {
         // 写文件
         writer << "<!DOCTYPE html>" << std::endl << "<html lang='zh-CN'><head><meta charset='UTF-8'><meta name='generator' content='HSC'><meta name='robots' content='noindex'><title>作业</title><link rel='shortcut icon' href='favicon.ico' type='image/x-icon'><link rel='stylesheet' href='style.css'></head>" << std::endl << "<body>";
-        std::string published_note = add_label("编辑于 " + outfile.published_time, "span");
+        string published_note = add_label("编辑于 " + outfile.published_time, "span");
         writer << published_note << std::endl;
-        std::string table = "<thead><tr><td colspan='3'>" + outfile.date + "作业</td></tr><tr><td>科目</td><td>序号</td><td>项目</td></tr></thead>";
-        std::string tbody = "";
+        string table = "<thead><tr><td colspan='3'>" + outfile.date + "作业</td></tr><tr><td>科目</td><td>序号</td><td>项目</td></tr></thead>";
+        string tbody = "";
         int subject_length = outfile.subjects.size();
         for (int i = 0; i < subject_length; i++) {
             Subject subject = outfile.subjects[i];
@@ -406,9 +397,9 @@ int main(int argc, char* argv[]) {
             Item item;
             if (item_length != 0) {
                 item = subject.items[0];
-                std::map<std::string, std::string> attrs;
+                std::map<string, string> attrs;
                 attrs["rowspan"] = std::to_string(item_length);
-                std::string td1 = add_label(subject.name, "td", attrs),
+                string td1 = add_label(subject.name, "td", attrs),
                     td2 = add_label(std::to_string(item.number), "td"),
                     td3 = add_label(item.text, "td");
                 tbody += add_label(td1 + td2 + td3, "tr", { {"class", "ll"} });
@@ -416,8 +407,8 @@ int main(int argc, char* argv[]) {
             }
             for (int j = 1; j < item_length; j++) {
                 item = subject.items[j];
-                std::string td1 = add_label(std::to_string(item.number), "td");
-                std::string td2 = add_label(item.text, "td");
+                string td1 = add_label(std::to_string(item.number), "td");
+                string td2 = add_label(item.text, "td");
                 tbody += add_label(td1 + td2, "tr", { {"class", "sl"} });
                 // sl: Short Line
             }
@@ -425,11 +416,11 @@ int main(int argc, char* argv[]) {
         table += add_label(tbody, "tbody");
         table = add_label(table, "table", { {"border", "1"} });
         writer << table << std::endl;
-        std::string create_modi_note = outfile.created_time + " 创建";
+        string create_modi_note = outfile.created_time + " 创建";
         int modi_length = outfile.modified_times.size();
         for (int i = 0; i < modi_length; i++) {
             Modification modi = outfile.modified_times[i];
-            create_modi_note += " / " + add_label(modi.modified_time + " 更正", "strong", { {"style", "--bgc:" + modi.bg_color} });
+            create_modi_note += " / " + add_label(modi.modified_time + " 修改", "strong", { {"style", "--bgc:" + modi.bg_color} });
         }
         create_modi_note = add_label(create_modi_note, "span");
         writer << create_modi_note << std::endl;
